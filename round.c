1770d2fea32f625bac5e2b9bcc7479760563b13c
Vaidooriya C H <vaidooriya2006@gmail.com>
2026-03-11 00:10:22 +0530

    Add files via upload

---
+++ round.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
+struct process {
+    char pid[10];
+    int at, bt, rt;
+    int ct, wt, tat;
+};
+
+int main() {
+    int n, q;
+    scanf("%d", &n);
+
+    struct process p[20];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
+        p[i].rt = p[i].bt;
+    }
+
+    scanf("%d", &q);
+
+    int time = 0, completed = 0;
+    int done[20] = {0};
+
+    while(completed < n) {
+        int executed = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(p[i].at <= time && p[i].rt > 0) {
+
+                executed = 1;
+
+                if(p[i].rt > q) {
+                    time += q;
+                    p[i].rt -= q;
+                }
+                else {
+                    time += p[i].rt;
+                    p[i].rt = 0;
+                    p[i].ct = time;
+                    completed++;
+                }
+            }
+        }
+
+        if(!executed)
+            time++;
+    }
+
+    float avg_wt = 0, avg_tat = 0;
+
+    for(int i = 0; i < n; i++) {
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        avg_wt += p[i].wt;
+        avg_tat += p[i].tat;
+    }
+
+    printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].wt);
+
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].tat);
+
+    printf("Average Waiting Time: %.2f\n", avg_wt/n);
+    printf("Average Turnaround Time: %.2f\n", avg_tat/n);
+
+    return 0;
+}
